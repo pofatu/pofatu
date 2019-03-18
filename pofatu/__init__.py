f1453ff2eba9aea4cf45f02df35ca445a1d10643
xrotwang <xrotwang@googlemail.com>
2019-03-18 11:34:18 +0100

    update

--- pofatu/__init__.py
+++ pofatu/__init__.py
@@ -4,6 +4,9 @@ from pyramid.config import Configurator
 from pofatu import models
 from pofatu.interfaces import ISite, IMeasurement
 
+from clld.web import app
+
+
 _ = lambda s: s
 _('Language')
 _('Languages')
@@ -27,4 +30,9 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('clldmpg')
     config.register_resource('measurement', models.Measurement, IMeasurement, with_index=True)
+    menuitems = config.registry.settings['clld.menuitems_list']
+    config.register_menu(
+        ('Artefacts', lambda ctx, req: (req.route_url('parameter', id='artefact'), 'Artefacts')),
+        ('Sources', lambda ctx, req: (req.route_url('parameter', id='source'), 'Sources')),
+        *menuitems)
     return config.make_wsgi_app()
